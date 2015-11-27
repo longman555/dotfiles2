724bb5c927c3e0042b197cab5d1151e3043b359c
longman555 <realgold555@gmail.com>
2015-11-28 05:47:18 +0900

    mod test.c

--- test.c
+++ test.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 
 int sum(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
 
 int main(void) {
 
     printf("Hello, world!\n");
     printf("1 + 2 = %d\n", sum(1, 2));
+    printf("4 - 3 = %d\n", sub(4, 3));
 
     return EXIT_SUCCESS;
 }
