a67d1bed5832b66d645f44f723711cb12c6d1650
longman555 <realgold555@gmail.com>
2015-11-28 05:26:00 +0900

    Revert "mod test.c"
    This reverts commit 890f072c89c74c0400aabde453ff08124c3ea48c.
    
    Just for test purpose.

--- test.c
+++ test.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sum(int a, int b) { return a + b; }
-
 int main(void) {
 
     printf("Hello, world!\n");
-    printf("1 + 2 = %d\n", sum(1, 2));
 
     return EXIT_SUCCESS;
 }
