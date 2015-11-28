8ce0e1c876147df5e1a61602f5b40381659e4c3a
longman555 <realgold555@gmail.com>
2015-11-29 03:32:29 +0900

    add t[1-2].txt; mod test.c

--- test.c
+++ test.c
@@ -4,10 +4,12 @@
 int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
+int dev(int a, int b);
 
 int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
 int mul(int a, int b) { return a * b; }
+int dev(int a, int b) { return a / b; }
 
 int main(void) {
 
@@ -15,6 +17,7 @@ int main(void) {
     printf("1 + 2 = %d\n", add(1, 2));
     printf("4 - 3 = %d\n", sub(4, 3));
     printf("5 * 6 = %d\n", mul(5, 6));
+    printf("8 / 2 = %d\n", dev(8, 2));
 
     return EXIT_SUCCESS;
 }
