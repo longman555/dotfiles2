13d7503fe9bee54abca479607a8d5e5045e2c538
longman555 <realgold555@gmail.com>
2015-11-29 02:28:57 +0900

    mod test.c

--- test.c
+++ test.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sum(int a, int b);
+int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
 
-int sum(int a, int b) { return a + b; }
+int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
 int mul(int a, int b) { return a * b; }
 
 int main(void) {
 
     printf("Hello, world!\n");
-    printf("1 + 2 = %d\n", sum(1, 2));
+    printf("1 + 2 = %d\n", add(1, 2));
     printf("4 - 3 = %d\n", sub(4, 3));
     printf("5 * 6 = %d\n", mul(5, 6));
 
