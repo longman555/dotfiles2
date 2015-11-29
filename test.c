39d3e91b9b81bd0bf46b363ef723b963fb1b13bc
longman555 <realgold555@gmail.com>
2015-11-30 05:49:26 +0900

    mod test.c(add more assertion)

--- test.c
+++ test.c
@@ -15,10 +15,16 @@ int dev(int a, int b) { return a / b; }
 
 void test_funcs(void) {
     assert(add(1, 2) == 3);
+    assert(add(2, 1) == 3);
+
     assert(sub(2, 3) == -1);
     assert(sub(3, 2) == 1);
+
     assert(mul(1, 2) == 2);
+    assert(mul(2, 1) == 2);
+
     assert(dev(6, 2) == 3);
+    assert(dev(2, 6) == 0);
 }
 
 int main(void) {
